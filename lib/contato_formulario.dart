2608cd18c4405ccd879cbdbb0499bd936857d5dd
josefelipeduarte <josefelipetoby@hotmail.com>
2023-05-03 21:30:53 -0300

    Update contato_formulario.dart

--- lib/contato_formulario.dart
+++ lib/contato_formulario.dart
@@ -36,27 +36,68 @@ class ContatoFormulario extends StatelessWidget {
   Widget _formUI(BuildContext context) {
     var _controller = TextEditingController();
     var _controller2 = TextEditingController();
-    return Column (children: <Widget>[
-      TextFormField(
-        controller: _controller, 
-        decoration: InputDecoration(
-        hintText: 'Nome Completo'),
-        maxLength: 100,
-        validator: _validarNome,
-        onSaved: (String? val) {
-          nome = val!;
-        },
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          controller: _controller,
+          decoration: InputDecoration(hintText: 'Nome Completo'),
+          maxLength: 100,
+          validator: _validarNome,
+          onSaved: (String? val) {
+            nome = val!;
+          },
         ),
         TextFormField(
-        controller: _controller2, 
-        decoration: InputDecoration(hintText: 'Idade'),
-        keyboardType: TextInputType.number,
-        maxLength: 3,
-        validator: _validarIdade, 
+            controller: _controller2,
+            decoration: InputDecoration(hintText: 'Idade'),
+            keyboardType: TextInputType.number,
+            maxLength: 3,
+            validator: _validarIdade,
+            onSaved: (String? val) {
+              idade = val!;
+            }),
+        SizedBox(height: 15.0),
+        ElevatedButton(onPressed: _sendForm, child: Text('Enviar')),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => MyContatoList()));
+          },
+          child: Text('Listar'),
         ),
-         onSaved: (String? val) {
-          idade = val!;
-        },
-    ])
+        ElevatedButton(
+            onPressed: () {
+              _controller.clear();
+              _controller2.clear();
+            },
+            child: Text('Limpar'))
+      ],
+    );
+  }
+
+  String? _validarNome(String? value) {
+    String patttern = r'(^[a-zA-Z]*$)';
+    RegExp regExp = RegExp(patttern);
+    if (value!.isEmpty) {
+      return "Informe o nome";
+    } else if (!regExp.hasMatch(value)) {
+      return "O nome deve conter caracteres de a-z ou A-Z";
+    }
+    return null;
+  }
+
+  String? _validarIdade(String? value) {
+    String patttern = r'(^[0-9]*$)';
+    RegExp regExp = RegExp(patttern);
+
+    if (value!.isEmpty) {
+      return "Informe a idade";
+    } else if (value.length != 2) {
+      return "A idade deve ter até 2 digitos";
+    } else if (!regExp.hasMatch(value)) {
+      return "A idade só deve conter números";
+    }
+    return null;
   }
 }
